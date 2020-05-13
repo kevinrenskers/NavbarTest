e48a5e0e12e6de6155c35c2c63c17b06d5d0c9c5
Kevin Renskers <kevin@loopwerk.io>
2020-05-13 13:11:57 +0200

    Just use transitionCoordinator directly

--- NavbarTest/ViewController.swift
+++ NavbarTest/ViewController.swift
@@ -1,32 +1,19 @@
 import UIKit
 
-class ColorTransitionNavigationController: UINavigationController {
-  func setBarTintColor(color: UIColor, animated: Bool, transitionCoordinator: UIViewControllerTransitionCoordinator?) {
-    guard let transitionCoordinator = transitionCoordinator, animated else {
-      return
-    }
-
-    transitionCoordinator.animateAlongsideTransition(in: view, animation: { [weak self] (context) in
-      self?.navigationBar.tintColor = color
-      self?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: color]
-    }, completion:nil)
-  }
-}
-
 class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
     navigationController?.navigationBar.shadowImage = UIImage()
     navigationController?.navigationBar.isTranslucent = true
-
-    navigationController?.navigationBar.tintColor = .black
   }
 
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
 
-    guard let navigationController = navigationController as? ColorTransitionNavigationController else { return }
-    navigationController.setBarTintColor(color: UIColor.black, animated: animated, transitionCoordinator: transitionCoordinator)
+    transitionCoordinator?.animate(alongsideTransition: { [weak self] (context) in
+      self?.navigationController?.navigationBar.tintColor = .black
+      self?.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black]
+    }, completion: nil)
   }
 }
--- NavbarTest/WhiteTitleViewController.swift
+++ NavbarTest/WhiteTitleViewController.swift
@@ -3,7 +3,10 @@ import UIKit
 class WhiteTitleViewController: UIViewController {
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
-    guard let navigationController = navigationController as? ColorTransitionNavigationController else { return }
-    navigationController.setBarTintColor(color: UIColor.white, animated: animated, transitionCoordinator: transitionCoordinator)
+
+    transitionCoordinator?.animate(alongsideTransition: { [weak self] (context) in
+      self?.navigationController?.navigationBar.tintColor = .white
+      self?.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+    }, completion: nil)
   }
 }
